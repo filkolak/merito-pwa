e214ba146270267f1b61b2e9fb82303229b79c8e
Filip Kołakowski <gdx174883@student.gdansk.merito.pl>
2025-04-12 09:31:48 +0200

    dopiszczenie styli

--- frontend/components/ProductListing.vue
+++ frontend/components/ProductListing.vue
@@ -18,18 +18,17 @@
                 {{ delivery }}
             </div>
 
-            <button class="w-full py-2 mt-2 bg-green-600 text-white rounded-md hover:bg-green-700 focus:outline-none">
+            <button class="w-full py-2 mt-2 bg-orange-500 text-white rounded-md hover:bg-orange-600 focus:outline-none">
                 DODAJ DO KOSZYKA
             </button>
 
-            <!-- Przycisk "Dodaj do ulubionych" z serduszkiem -->
+            <!-- Ikona serduszka po prawej stronie -->
             <button
-                class="w-full py-2 mt-2 bg-gray-100 text-gray-700 rounded-md hover:bg-gray-200 focus:outline-none flex items-center justify-center">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 mr-2">
+                class="w-full py-2 mt-2 bg-gray-700 text-white rounded-md hover:bg-gray-800 focus:outline-none flex items-center justify-center">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 ml-2">
                     <path
                         d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
                 </svg>
-                Dodaj do ulubionych
             </button>
         </div>
     </div>
