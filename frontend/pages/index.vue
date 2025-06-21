adab0cfd553acb7551e60abe99a5148c20e1fca2
Filip Kołakowski <filipk@creativia.com.pl>
2025-06-21 08:46:35 +0200

    poprawa wyświetlania produktu

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -11,19 +11,21 @@ const inspiracje = computed(() => data.value?.inspiracje || [])
 </script>
 
 <template>
-  <BasicSlider />
-  <!-- Pasek kategorii -->
-  <CategoryScroller :categories="categories" />
+      <BasicSlider/>
+      <!-- Pasek kategorii -->
+      <CategoryScroller :categories="categories" />
 
-  <!-- Sekcja 1 Banner -->
-  <HomeSection title="Jak niskie ceny, to czas na wiosenne zakupy" :products="produkty1" />
+      <!-- Sekcja 1 Banner -->
+      <HomeSection title="Jak niskie ceny, to czas na wiosenne zakupy" :products="produkty1" />
 
-  <!-- Sekcja 2 -->
-  <HomeSection title="Na czasie" :products="produkty2" />
-  <!-- Sekcja z inspiracjami -->
-  <InspirationBanner :items="inspiracje" />
-  <HomeSection title="Gwarancja najniższej ceny w Domu i ogrodzie" :products="produkty3" />
-  <HomeSection title="Kupuj z szybką dostawą" :products="produkty4" />
+      <!-- Sekcja 2 -->
+      <HomeSection title="Na czasie" :products="produkty2" />
+      <!-- Sekcja z inspiracjami -->
+      <InspirationBanner :items="inspiracje" />
+      <HomeSection title="Gwarancja najniższej ceny w Domu i ogrodzie" :products="produkty3" />
+      <HomeSection title="Kupuj z szybką dostawą" :products="produkty4" />
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
